81aaa6a7decb8e0af809a7e4f5c206951482ccc8
Lindell86 <sked86@hotmail.com>
2014-06-12 16:54:40 +0200

    created scripts folder

---
+++ Game/Assets/Scrips/MusicPlayer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour
+{
+
+    public AudioClip BackgroundMusic;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (BackgroundMusic)
+        {
+            audio.clip = BackgroundMusic;
+            audio.Play();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
