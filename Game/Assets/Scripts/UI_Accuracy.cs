6399c5ec5bf3a2694b78674a0bdf53b10cb6f4bf
Bullda <bullda_94@hotmail.com>
2014-06-12 20:22:49 +0200

    camera ui, AGAIN

--- Game/Assets/Scripts/UI_Accuracy.cs
+++ Game/Assets/Scripts/UI_Accuracy.cs
@@ -2,14 +2,14 @@
 using System.Collections;
 
 public class UI_Accuracy : MonoBehaviour {
-
+    public int TextSize = 4;
 	// Use this for initialization
 	void Start () {
-	
+        gameObject.guiText.fontSize = TextSize;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        gameObject.guiText.text = "Accuracy: " + (Globals.Accuracy * 100).ToString();
 	}
 }
