99e286e19eb3cc0715db83187620309633e3da94
Lindell86 <sked86@hotmail.com>
2014-06-12 22:11:59 +0200

    scprits changes

--- Game/Assets/Scripts/ShipOverworldMovement.cs
+++ Game/Assets/Scripts/ShipOverworldMovement.cs
@@ -19,25 +19,29 @@ public class ShipOverworldMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update() {
-	    if(Vector3.Distance(transform.position, Destination) > 20.0f)
+    void Update()
+    {
+        if (Globals.state == Globals.GameState.OVERWORLD)
         {
-            Vector3 direction = Destination - transform.position;
-            direction.Normalize();
-            direction *= Movespeed;
-            direction *= Time.deltaTime;
+            if (Vector3.Distance(transform.position, Destination) > 20.0f)
+            {
+                Vector3 direction = Destination - transform.position;
+                direction.Normalize();
+                direction *= Movespeed;
+                direction *= Time.deltaTime;
 
-            Vector3 newPos = transform.position;
-            newPos.x += direction.x;
-            newPos.y += direction.y;
-            transform.position = newPos;
-            Debug.Log(transform.position);
+                Vector3 newPos = transform.position;
+                newPos.x += direction.x;
+                newPos.y += direction.y;
+                transform.position = newPos;
+                Debug.Log(transform.position);
+            }
+            if (Input.GetMouseButtonDown(0))
+            {
+                SetNewDestination();
+            }
         }
-        if(Input.GetMouseButtonDown(0))
-        {
-            SetNewDestination();
-        }
-	}
+    }
 
     void SetNewDestination()
     {
