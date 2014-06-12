237f56ffd251e3e546eccc1100928b0ccd5b2200
Bullda <bullda_94@hotmail.com>
2014-06-12 21:03:32 +0200

    added text for evasion

---
+++ Game/Assets/UI_Evasion.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UI_Evasion : MonoBehaviour {
+    public int TextSize = 18;
+	// Use this for initialization
+	void Start () {
+        gameObject.guiText.fontSize = TextSize;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.guiText.text = "Evasion: " + (Globals.Evasion * 100).ToString();
+	}
+}
