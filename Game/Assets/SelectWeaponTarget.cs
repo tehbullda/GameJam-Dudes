817e7a6ded761e34da38a7060b1ef0b24f1a74c7
Bullda <bullda_94@hotmail.com>
2014-06-12 19:38:02 +0200

    UI - added basic support for aiming

---
+++ Game/Assets/SelectWeaponTarget.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectWeaponTarget : MonoBehaviour {
+    public Texture Inactive;
+    public Texture Active;
+	// Use this for initialization
+	void Start () {
+        renderer.material.mainTexture = Inactive;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!Globals.GamePaused && renderer.material.mainTexture != Inactive) {
+            renderer.material.mainTexture = Inactive;
+        }
+	}
+
+    void OnMouseDown() {
+        Debug.Log("Aiming Engaged");
+        renderer.material.mainTexture = Active;
+    }
+}
